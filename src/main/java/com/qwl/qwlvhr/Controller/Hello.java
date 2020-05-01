a16186f8fce6f05aa6f911f7cffd7fdbd8927149
QiWeiLong <291532919@qq.com>
2020-05-01 15:19:25 +0800

    第二次提交，git代码提交测试

--- src/main/java/com/qwl/qwlvhr/Controller/Hello.java
+++ src/main/java/com/qwl/qwlvhr/Controller/Hello.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
+    //写个注解做一些改动，进行二次提交
     @GetMapping("/hello")
     public String getHello(){
         return "hello~~~~~";
--- src/main/java/com/qwl/qwlvhr/mapper/HrMapper.java
+++ src/main/java/com/qwl/qwlvhr/mapper/HrMapper.java
@@ -1,7 +1,7 @@
 package com.qwl.qwlvhr.mapper;
 
 import com.qwl.qwlvhr.bean.Hr;
-
+//随便写写
 public interface HrMapper {
 
     Hr getHrByUsername(String username);
